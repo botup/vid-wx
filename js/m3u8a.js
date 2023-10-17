38b0e02c0d2aa93f1e998f926ee0f4bc30cb794a
botup <130203439+botup@users.noreply.github.com>
2023-10-17 21:50:11 +0800

    Update m3u8a.js

--- js/m3u8a.js
+++ js/m3u8a.js
@@ -43,6 +43,6 @@ var m20231010003 = "https://resource.luoye.icu/upload/cache/Lemon/1696915221156.
 var m20231017001 = "https://resource.luoye.icu/upload/cache/Lemon/1697550119820.m3u8"
 var m20231017002 = "https://resource.luoye.icu/upload/cache/Lemon/1697550229623.m3u8"
 var m20231017003 = "https://resource.luoye.icu/upload/cache/Lemon/1697550449646.m3u8"
-var m20231017004 = ""
+var m20231017004 = "https://resource.luoye.icu/upload/cache/Lemon/1697550592771.m3u8"
 var m20231017005 = ""
 var m20231017006 = ""
