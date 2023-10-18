515215eb334c8d6cc9a8e77582a5d336c84a100b
botup <130203439+botup@users.noreply.github.com>
2023-10-18 11:49:17 +0800

    Update qun.js

--- js/qun.js
+++ js/qun.js
@@ -1,2 +1,2 @@
 var titlequn = "更多资源加Q群:582774486"
-var qqqun = "https://botup.github.io/gof"
+var qqqun = "https://c.pc.qq.com/middlect.html?iscontinue=1&pfurl=https://sharechain.qq.com/c120f975317326b8107e305ddd47cc71"
