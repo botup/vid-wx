57da3ad0e8a46c444d779eefe9a36e5c272d45dc
botup <130203439+botup@users.noreply.github.com>
2024-02-14 15:21:45 +0800

    Update m3u8a.js

--- js/m3u8a.js
+++ js/m3u8a.js
@@ -1,54 +1,6 @@
 // m3u8 url 变量命名格式：'m' + 当前所在的目录名 + 当前所在的文件名
 var m20230930001 = "https://resource.luoye.icu/upload/cache/Lemon/1696080442102.m3u8"
 var m20230930002 = "https://resource.luoye.icu/upload/cache/Lemon/1696080487468.m3u8"
-var m20230930003 = "https://resource.luoye.icu/upload/cache/Lemon/1696086731147.m3u8"
-var m20230930004 = "https://resource.luoye.icu/upload/cache/Lemon/1696087655792.m3u8"
-var m20230930005 = "https://resource.luoye.icu/upload/cache/Lemon/1696088432310.m3u8"
-
-var m20231001001 = "https://resource.luoye.icu/upload/cache/Lemon/1696134732593.m3u8"
-var m20231001002 = "https://resource.luoye.icu/upload/cache/Lemon/1696130569611.m3u8"
-var m20231001003 = "https://resource.luoye.icu/upload/cache/Lemon/1696152649451.m3u8"
-var m20231001004 = "https://resource.luoye.icu/upload/cache/Lemon/1696140890964.m3u8"
-var m20231001005 = "https://resource.luoye.icu/upload/cache/Lemon/1696350294368.m3u8"
-var m20231001006 = "https://resource.luoye.icu/upload/cache/Lemon/1696140961536.m3u8"
-var m20231001007 = "https://resource.luoye.icu/upload/cache/Lemon/1696164824936.m3u8"
-var m20231001008 = "https://resource.luoye.icu/upload/cache/Lemon/1696165639478.m3u8"
-
-var m20231002001 = "https://resource.luoye.icu/upload/cache/Lemon/1696213364025.m3u8"
-var m20231002002 = "https://resource.luoye.icu/upload/cache/Lemon/1696213676271.m3u8"
-var m20231002003 = "https://resource.luoye.icu/upload/cache/Lemon/1696247758221.m3u8"
-var m20231002004 = "https://resource.luoye.icu/upload/cache/Lemon/1696260170600.m3u8"
-var m20231002005 = "https://resource.luoye.icu/upload/cache/Lemon/1696255674960.m3u8"
-var m20231002006 = "https://resource.luoye.icu/upload/cache/Lemon/1696255992753.m3u8"
-
-var m20231003001 = "https://resource.luoye.icu/upload/cache/Lemon/1696323795988.m3u8"
-var m20231003002 = "https://resource.luoye.icu/upload/cache/Lemon/1696331210986.m3u8"
-var m20231003003 = "https://resource.luoye.icu/upload/cache/Lemon/1696343695382.m3u8"
-var m20231003004 = "https://resource.luoye.icu/upload/cache/Lemon/1696346996182.m3u8"
-var m20231003005 = "https://resource.luoye.icu/upload/cache/Lemon/1696347350376.m3u8"
-
-var m20231004001 = "https://resource.luoye.icu/upload/cache/Lemon/1696387095792.m3u8"
-var m20231004002 = "https://resource.luoye.icu/upload/cache/Lemon/1696401721162.m3u8"
-var m20231004003 = "https://resource.luoye.icu/upload/cache/Lemon/1696404460237.m3u8"
-var m20231004004 = "https://resource.luoye.icu/upload/cache/Lemon/1696427384832.m3u8"
-
-var m20231006001 = "https://resource.luoye.icu/upload/cache/Lemon/1696565262289.m3u8"
-
-var m20231008001 = "https://resource.luoye.icu/upload/cache/Lemon/1696742739230.m3u8"
-
-var m20231010001 = "https://resource.luoye.icu/upload/cache/Lemon/1696914897130.m3u8"
-var m20231010002 = "https://resource.luoye.icu/upload/cache/Lemon/1696915129614.m3u8"
-var m20231010003 = "https://resource.luoye.icu/upload/cache/Lemon/1696915221156.m3u8"
 
 var m20231017001 = "https://resource.luoye.icu/upload/cache/Lemon/1697550119820.m3u8"
 var m20231017002 = "https://resource.luoye.icu/upload/cache/Lemon/1697550229623.m3u8"
-var m20231017003 = "https://resource.luoye.icu/upload/cache/Lemon/1697550449646.m3u8"
-var m20231017004 = "https://resource.luoye.icu/upload/cache/Lemon/1697550592771.m3u8"
-var m20231017005 = "https://resource.luoye.icu/upload/cache/Lemon/1697550830913.m3u8"
-var m20231017006 = "https://resource.luoye.icu/upload/cache/Lemon/1697550888898.m3u8"
-
-var m20231018001 = "https://resource.luoye.icu/upload/cache/Lemon/1697610822032.m3u8"
-
-var m20231021001 = ""
-var m20231021002 = ""
-var m20231021003 = ""
