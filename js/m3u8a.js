652a2605a62ba3543a1e09d29d16b341d119d793
botup <130203439+botup@users.noreply.github.com>
2024-02-14 15:26:20 +0800

    Update m3u8a.js

--- js/m3u8a.js
+++ js/m3u8a.js
@@ -1,6 +1,4 @@
 // m3u8 url 变量命名格式：'m' + 当前所在的目录名 + 当前所在的文件名
 var m20230930001 = "https://resource.luoye.icu/upload/cache/Lemon/1696080442102.m3u8"
-var m20230930002 = "https://resource.luoye.icu/upload/cache/Lemon/1696080487468.m3u8"
 
 var m20231017001 = "https://resource.luoye.icu/upload/cache/Lemon/1697550119820.m3u8"
-var m20231017002 = "https://resource.luoye.icu/upload/cache/Lemon/1697550229623.m3u8"
